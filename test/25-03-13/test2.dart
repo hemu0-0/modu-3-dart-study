66023b4390f6985fb339267c28b8ed29fe2bde7d
Hwamok Lee <ghkahr1890@gmail.com>
2025-03-13 17:30:36 +0900

    expect test

--- test/25-03-13/test2.dart
+++ test/25-03-13/test2.dart
@@ -2,11 +2,13 @@ import 'package:modu_3_dart_study/25-03-13/cleric.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Cleric Test' , () {
-    final cleric = Cleric('성자', 50, 1);
+  test('Cleric Test', () {
+    int value = 10;
 
-    int mprecover = cleric.pray(3);
+    print('Testing value: $value');
 
-  expect(cleric.mp, equals(mprecover));
- });
+    expect(value, greaterThan(5));
+    expect(value, lessThan(15));
+    expect(value, inInclusiveRange(5, 15));
+  });
 }
